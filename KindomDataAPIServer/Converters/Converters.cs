8351e29e139840c9e3bfdb9bb91033a6e37ef6f8
gsh <1214585404@qq.com>
2026-07-17 16:30:35 +0800

    fix

--- KindomDataAPIServer/Converters/Converters.cs
+++ KindomDataAPIServer/Converters/Converters.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace KindomDataAPIServer.Converters
@@ -20,6 +21,34 @@ namespace KindomDataAPIServer.Converters
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value?.Equals(true) == true ? parameter : Binding.DoNothing;
+        }
+    }
+
+    public class DecimalToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || value == DependencyProperty.UnsetValue)
+            {
+                return null;
+            }
+
+            return System.Convert.ToDecimal(value, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || value == DependencyProperty.UnsetValue)
+            {
+                return Binding.DoNothing;
+            }
+
+            if (value is string text && string.IsNullOrWhiteSpace(text))
+            {
+                return Binding.DoNothing;
+            }
+
+            return System.Convert.ToInt32(value, culture);
         }
     }
 }
